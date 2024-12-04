a464a5d678e65895dfca984e286f771d6a50a07d
Mohammadasad098 <mohammadasadiam64@gmail.com>
2024-12-04 22:07:52 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import connectDB from "./src/db/index.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import userRoutes from "./src/routes/users.routes.js";
+
+
+const app = express()
+
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+
+app.use("/api/v1", userRoutes);
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+connectDB()
+
+.then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`⚙️  Server is running at port : ${process.env.PORT}`);
+  });
+})
+.catch((err) => {
+  console.log("MONGO DB connection failed !!! ", err);
+});
